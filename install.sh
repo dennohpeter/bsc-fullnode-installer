a3a9c28c6dc3b8a11751adcd8f07349e8aae00b4
Dennoh Peter <denispeterson96@gmail.com>
2022-10-05 21:20:25 +0300

    Update and rename install_fullnode.sh to install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,10 @@ echo "Credits - PhatJay for those steps."
 # Download NPM and PM2.
 echo "Installing NPM and PM2..."
 apt update
-apt install nodejs npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 17
 npm install pm2 -g
 
 # Download unzip.
