5acb2f257f75c3c56e9fc93d382d44ae4e41afd6
Dennoh Peter <denispeterson96@gmail.com>
2023-01-08 01:14:29 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ wget -O /home/geth/geth_linux https://github.com/binance-chain/bsc/releases/late
 chmod +x /home/geth/geth_linux
 
 # Create the start.sh.
-echo "./geth_linux --config ./config.toml --datadir ./mainnet --cache 18000 --rpc.allow-unprotected-txs --txlookuplimit 0 --http --maxpeers 100 --ws --syncmode=full --snapshot=false  --pipecommit --diffsync" >  /home/geth/start.sh
+echo "./geth_linux --config ./config.toml --datadir ./mainnet --cache 8000 --rpc.allow-unprotected-txs --txlookuplimit 0 --tries-verify-mode none
 
 chmod +x /home/geth/start.sh
 
